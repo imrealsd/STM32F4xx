482f782efd213cd8e83d3b01e12813a0fdf4a6aa
Subhadeep <subhadeepdhang1@gmail.com>
2022-10-08 09:01:32 +0530

    modified upload script

--- 15.RTC_ON_CHIP/RTC_ON_CHIP/upload.sh
+++ 15.RTC_ON_CHIP/RTC_ON_CHIP/upload.sh
@@ -1,9 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "STARTING UPLOAD PROCESS"
 echo "----------------------------------------------------------------"
 cd .pio/build/genericSTM32F407VET6
-sudo dfu-util -a 0 -i 0 -s 0x08000000:leave -D firmware.bin
+sudo su
+dfu-util -a 0 -i 0 -s 0x08000000:leave -D firmware.bin
+exit
 
 echo "-----------------------------------------------------------------"
 echo "FIRMWARE UPLOADED SUCCESSFULLY"
