9b3615a84348a83b09c8a624b1590a6c77d49aee
Subhadeep <subhadeepdhang1@gmail.com>
2023-01-03 18:07:46 +0530

    configured iwdg prescaler

--- 13.WDG/IWDG-Test/src/iwdg.c
+++ 13.WDG/IWDG-Test/src/iwdg.c
@@ -20,9 +20,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "iwdg.h"
 
-/* USER CODE BEGIN 0 */
 
-/* USER CODE END 0 */
 
 IWDG_HandleTypeDef hiwdg;
 
@@ -30,26 +28,13 @@ IWDG_HandleTypeDef hiwdg;
 void MX_IWDG_Init(void)
 {
 
-  /* USER CODE BEGIN IWDG_Init 0 */
-
-  /* USER CODE END IWDG_Init 0 */
-
-  /* USER CODE BEGIN IWDG_Init 1 */
-
-  /* USER CODE END IWDG_Init 1 */
   hiwdg.Instance = IWDG;
-  hiwdg.Init.Prescaler = IWDG_PRESCALER_32;
-  hiwdg.Init.Reload = 4095;
+  /* [32 KHZ LSI / 32 = 1 KHZ CLK to IWDG , Time-Period = 1ms ]*/
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_32;  
+  /*MAX Val 4095 [12 bit], MAX time delay = 4095*1 ms = 4.095 sec*/
+  hiwdg.Init.Reload = 4000;  
   if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN IWDG_Init 2 */
-
-  /* USER CODE END IWDG_Init 2 */
-
 }
-
-/* USER CODE BEGIN 1 */
-
-/* USER CODE END 1 */
