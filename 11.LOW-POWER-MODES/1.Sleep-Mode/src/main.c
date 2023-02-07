d374fcabf1fe97b4ec37c6ef35ec879a532be3f5
Subhadeep <subhadeepdhang1@gmail.com>
2023-02-07 13:20:56 +0530

    added main code ... tested ok

--- 11.LOW-POWER-MODES/1.Sleep-Mode/src/main.c
+++ 11.LOW-POWER-MODES/1.Sleep-Mode/src/main.c
@@ -22,7 +22,7 @@
 #include "gpio.h"
 #include <string.h>
 
-char uartRxData[1] = {0};
+uint8_t uartRxData[1];
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
@@ -36,15 +36,12 @@ void MCU_TaskAfterWakeup(void);
  */
 int main(void)
 {
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 	HAL_Init();
-	/* Configure the system clock */
 	SystemClock_Config();
-	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
-	/*Initialize the uart & put it into receive_it mode for 1 byte reception*/
 	MX_USART1_UART_Init();
 	HAL_UART_Receive_IT(&huart1,(uint8_t *)uartRxData, 1);
+	HAL_Delay(2000);
 
 	while (1){
 		
@@ -57,10 +54,14 @@ int main(void)
 
 void MCU_EnterSleepMode_WFI(void)
 {	
-	char *sleepMsg = "Going into Sleep Mode :-) \n";
-	HAL_SuspendTick();
+	char *sleepMsg = "Going into Sleep Mode in 5 sec\r\n";
+
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
 	HAL_UART_Transmit(&huart1, (uint8_t *)sleepMsg, strlen(sleepMsg), 100);
+	HAL_Delay(5000);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+
+	HAL_SuspendTick();
 	HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
 }
 
@@ -68,8 +69,10 @@ void MCU_EnterSleepMode_WFI(void)
 
 void MCU_TaskAfterWakeup(void)
 {	
-	char *wakeupMsg = "Wake up to Reailty :-( \n";
+	char *wakeupMsg = "Wake up to Reailty !!\r\n";
+
 	HAL_ResumeTick();
+	HAL_UART_Transmit(&huart1, (uint8_t *)wakeupMsg, strlen(wakeupMsg), 100);
 
 	for (volatile int i = 0; i < 10; i++){
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
@@ -80,6 +83,13 @@ void MCU_TaskAfterWakeup(void)
 }
 
 
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{	
+	char *uartMsg = "Here Comes The UART-Interrupt\r\n";
+    HAL_UART_Receive_IT(huart,(uint8_t *)uartRxData, 1);
+    HAL_UART_Transmit(&huart1, (uint8_t *)uartMsg, strlen(uartMsg), 100);
+}
+
 
 /**
  * @brief System Clock Configuration
@@ -87,40 +97,44 @@ void MCU_TaskAfterWakeup(void)
  */
 void SystemClock_Config(void)
 {
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	__HAL_RCC_PWR_CLK_ENABLE();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 4;
-	RCC_OscInitStruct.PLL.PLLN = 168;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 4;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK){
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK){
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
