a8b0defea36c923e249f6aaa7ff3c5fdac68d3c8
Subhadeep <subhadeepdhang1@gmail.com>
2023-02-07 13:20:28 +0530

    changed default state of led pin

--- 11.LOW-POWER-MODES/1.Sleep-Mode/src/gpio.c
+++ 11.LOW-POWER-MODES/1.Sleep-Mode/src/gpio.c
@@ -50,7 +50,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
 
   /*Configure GPIO pin : PA1 */
   GPIO_InitStruct.Pin = GPIO_PIN_1;
