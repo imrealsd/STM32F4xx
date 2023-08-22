60918406f06c1aae5d0ee24e6a4ddd3cd8900acf
Subhadeep <subhadeepdhang1@gmail.com>
2023-08-22 20:24:05 +0530

    added own bmp280  driver

--- 7.I2C/3.BMP280-I2C/Core/Inc/bmp280.h
+++ 7.I2C/3.BMP280-I2C/Core/Inc/bmp280.h
@@ -1,15 +1,83 @@
 #ifndef BMP280_H
 #define BMP280_H
 
+/*Includes*/
 #include "stm32f4xx_hal.h"
 #include "i2c.h"
 
-uint8_t bmp280_init(void);		     // call this first
-uint8_t bmp280_get_status(void);	 // read the status register
-void bmp280_set_config(uint8_t t_sb, uint8_t filter, uint8_t spi3w_en);  // set the configuration register      
-void bmp280_set_ctrl(uint8_t osrs_t, uint8_t osrs_p, uint8_t mode);      // set the oversampling and mode (this starts the conversion)
-float bmp280_getpressure(void);      // return prssure value in hpa     
-float bmp280_gettemperature(void);   // return temparature value in celcius
-double bmp280_getaltitude(void);     // return altutude value in meters
+/*Driver Macros*/
+
+#define BMP280_ADDR	            0x76 // 0x77
+
+#define BMP280_ID_REG		    0xD0
+#define BMP280_ID_VAL		    0x58
+
+#define BMP280_CAL_REG_FIRST	0x88
+#define BMP280_CAL_REG_LAST	    0xA1
+#define BMP280_CAL_DATA_SIZE	(BMP280_CAL_REG_LAST - BMP280_CAL_REG_FIRST + 1)
+
+#define BMP280_STATUS_REG	    0xF3
+#define BMP280_CONTROL_REG	    0xF4
+#define BMP280_CONFIG_REG	    0xF5
+
+#define BMP280_PRES_REG		    0xF7
+#define BMP280_TEMP_REG		    0xFA
+#define BMP280_RAWDATA_BYTES    6	  
+
+
+/*Driver APIs*/
+
+/**
+ * @brief initialize bmp280 sensor
+ * @param none
+ * @retval 1 (success) / 0 (faliure)
+ */
+uint8_t bmp280_init(void);	
+
+/**
+ * @brief get bmp280 status
+ * @param none
+ * @retval status register value 
+ */
+uint8_t bmp280_get_status(void);	
+
+/**
+ * @brief set bmp280 config register
+ * @param t_sb standby duration 
+ * @param filter time constant of IIR filter
+ * @param spi3w_en 3 wire spi mode enable
+ * @retval none
+ */
+void bmp280_set_config(uint8_t t_sb, uint8_t filter, uint8_t spi3w_en);  
+
+/**
+ * @brief set bmp280 control register
+ * @param osrs_t oversampling of temparature data
+ * @param osrs_p oversampling of pressure data
+ * @param mode power mode of bmp280
+ * @retval 
+ */
+void bmp280_set_ctrl(uint8_t osrs_t, uint8_t osrs_p, uint8_t mode); 
+
+/**
+ * @brief get pressure value
+ * @param none
+ * @retval pressure in hpa unit
+ */
+float bmp280_get_pressure(void);    
+
+/**
+ * @brief get temperature value
+ * @param none
+ * @retval temperature in celcius unit
+ */
+float bmp280_get_temperature(void);   
+
+/**
+ * @brief get alitude value
+ * @param none
+ * @retval altitude in meter unit
+ */
+double bmp280_get_altitude(void);     
 
 #endif
--- 7.I2C/3.BMP280-I2C/Core/Src/bmp280.c
+++ 7.I2C/3.BMP280-I2C/Core/Src/bmp280.c
@@ -3,43 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdint.h>
 
 
-#define BMP280_ADDR	0x76	// can be 0x77 or 0x76
-
-#define BMP280_ID_REG		    0xD0
-#define BMP280_ID_VAL		    0x58
-
-#define BMP280_CAL_REG_FIRST	0x88
-#define BMP280_CAL_REG_LAST	    0xA1
-#define BMP280_CAL_DATA_SIZE	(BMP280_CAL_REG_LAST+1 - BMP280_CAL_REG_FIRST)
-
-#define BMP280_STATUS_REG	    0xF3
-#define BMP280_CONTROL_REG	    0xF4
-#define BMP280_CONFIG_REG	    0xF5
-
-#define BMP280_PRES_REG		    0xF7
-#define BMP280_TEMP_REG		    0xFA
-#define BMP280_RAWDATA_BYTES       6	// 3 bytes pressure, 3 bytes temperature
-
-// Global variables for the sake of simplicity.
-int32_t _bmp280_temp;
-uint32_t _bmp280_pres;
-
-static void bmp280_writemem(uint8_t regAddress, uint8_t regValue)
-{
-    HAL_I2C_Mem_Write(&hi2c2, (BMP280_ADDR << 1), regAddress, 1, &regValue, 1, HAL_MAX_DELAY);
-}
+static struct bmp280_data_structure {
+    uint32_t pressure;
+    int32_t temperature;
+} bmp280_data;
 
 
-void bmp280_readmem(uint8_t regAddress, uint8_t rxBuff[], uint8_t rxBuffSize)
-{
-    HAL_I2C_Mem_Read(&hi2c2, (BMP280_ADDR << 1), regAddress, 1, rxBuff, rxBuffSize, HAL_MAX_DELAY);
-}
-
-// we create a struct for this, so that we can add support for two
-// sensors on a I2C bus
-static union _bmp280_cal_union {
+static union bmp280_calibration_union {
 	uint8_t bytes[BMP280_CAL_DATA_SIZE];
 	struct {
 		uint16_t dig_t1;
@@ -57,92 +30,133 @@ static union _bmp280_cal_union {
 	};
 } bmp280_cal;
 
-/*
- * read calibration registers
- */
-static void bmp280_getcalibration(void)
-{
-	memset(bmp280_cal.bytes, 0, sizeof(bmp280_cal));
 
-	bmp280_readmem(
-		BMP280_CAL_REG_FIRST,
-		bmp280_cal.bytes,
-		BMP280_CAL_DATA_SIZE
-	);
-}
+/*private function protorypes*/
+static void bmp280_measure(void);
+static void bmp280_read_mem(uint8_t reg_address, uint8_t rx_buff[], uint8_t buff_size);
+static void bmp280_write_mem(uint8_t reg_address, uint8_t reg_Value);
+static void bmp280_get_calibration(void);
+static int32_t bmp280_pack_20bit(uint8_t byte1, uint8_t byte2, uint8_t byte3);
+
+
+/*Function Definitations*/
 
 uint8_t bmp280_init(void)
 {
-	uint8_t buffer[1];
+	uint8_t id_reg_val;
 	HAL_Delay(10);
-	// look up the ID register
-	buffer[0] = 0;
-	bmp280_readmem(BMP280_ID_REG, buffer, 1);
-	if (buffer[0] != BMP280_ID_VAL)
+    
+	/*Check the ID register*/
+	id_reg_val = 0;
+	bmp280_read_mem(BMP280_ID_REG, &id_reg_val, 1);
+	if (id_reg_val != BMP280_ID_VAL)
 		return 0;
 
-	bmp280_getcalibration();
-	bmp280_set_config(0, 4, 0); // 0.5 ms delay, 16x filter, no 3-wire SPI
-	bmp280_set_ctrl(2, 5, 3); // T oversample x2, P over x2, normal mode
+	bmp280_get_calibration();
 
+    /* 0.5 ms delay, 16x filter, no 3-wire SPI */
+	bmp280_set_config(0, 4, 0); 
+    /* T oversample x2, P over x2, normal mode */
+	bmp280_set_ctrl(2, 5, 3); 
 	return 1;
 }
 
 uint8_t bmp280_get_status(void)
 {
-	uint8_t data[1];
-	bmp280_readmem(BMP280_STATUS_REG, data, 1);
-	return data[0];
+	uint8_t status_reg_val;
+	bmp280_read_mem(BMP280_STATUS_REG, &status_reg_val, 1);
+	return status_reg_val;
+}
+
+float bmp280_get_temperature(void) 
+{
+	bmp280_measure();
+	return (float)bmp280_data.temperature / 100;
+}
+
+float bmp280_get_pressure(void) 
+{
+	bmp280_measure();
+	return (float)bmp280_data.pressure / 100;
+}
+
+double bmp280_get_altitude(void) 
+{
+	double alt;
+	bmp280_measure();
+	alt = (1 - pow(bmp280_data.pressure/(double)101325, 0.1903)) / 0.0000225577;
+	return (alt/10);
 }
 
 void bmp280_set_ctrl(uint8_t osrs_t, uint8_t osrs_p, uint8_t mode)
-{
-	bmp280_writemem(BMP280_CONTROL_REG,
-		((osrs_t & 0x7) << 5)
-		| ((osrs_p & 0x7) << 2)
-		| (mode & 0x3)
-	);
+{   
+    uint8_t ctrl_reg_val;
+    ctrl_reg_val = ((osrs_t & 0x7) << 5) | ((osrs_p & 0x7) << 2) | (mode & 0x3);
+	bmp280_write_mem(BMP280_CONTROL_REG, ctrl_reg_val);
 }
+
 
 void bmp280_set_config(uint8_t t_sb, uint8_t filter, uint8_t spi3w_en)
-{
-	bmp280_writemem(BMP280_CONFIG_REG,
-		((t_sb & 0x7) << 5)
-		| ((filter & 0x7) << 2)
-		| (spi3w_en & 1)
-	);
+{   
+    uint8_t conf_reg_val;
+    conf_reg_val = ((t_sb & 0x7) << 5)| ((filter & 0x7) << 2)| (spi3w_en & 1);
+	bmp280_write_mem(BMP280_CONFIG_REG, conf_reg_val);
 }
 
-#define bmp280_20bit_reg(b1, b2, b3)	( \
-	((int32_t)(b1) << 12) \
-	| ((int32_t)(b2) << 4) \
-	| ((int32_t)(b3) >> 4) \
-)
-	
-void bmp280_measure(void)
+
+static int32_t bmp280_pack_20bit(uint8_t byte1, uint8_t byte2, uint8_t byte3)
 {
-	uint8_t data[BMP280_RAWDATA_BYTES];
-	int32_t temp_raw, pres_raw,
-		var1, var2, t_fine;
+    int32_t data;
+    data = (byte1 << 12) | (byte2 << 4) | ((byte3 & 0xF0) >> 4);
+    return data;
+}
+
+
+static void bmp280_get_calibration(void)
+{
+	memset(bmp280_cal.bytes, 0, sizeof(bmp280_cal));
+	bmp280_read_mem( BMP280_CAL_REG_FIRST, bmp280_cal.bytes,BMP280_CAL_DATA_SIZE);
+}
+
+
+static void bmp280_write_mem(uint8_t reg_address, uint8_t reg_Value)
+{
+    HAL_I2C_Mem_Write(&hi2c2, (BMP280_ADDR << 1), reg_address, 1, &reg_Value, 1, HAL_MAX_DELAY);
+}
+
+
+static void bmp280_read_mem(uint8_t reg_address, uint8_t rx_buff[], uint8_t buff_size)
+{
+    HAL_I2C_Mem_Read(&hi2c2, (BMP280_ADDR << 1), reg_address, 1, rx_buff, buff_size, HAL_MAX_DELAY);
+}
+
 	
-	// read the raw ADC data from the I2C registers
-	bmp280_readmem(BMP280_PRES_REG, data, BMP280_RAWDATA_BYTES);
-	pres_raw = bmp280_20bit_reg(data[0], data[1], data[2]);
-	temp_raw = bmp280_20bit_reg(data[3], data[4], data[5]);
+static void bmp280_measure(void)
+{
+	uint8_t raw_data[BMP280_RAWDATA_BYTES];
+	int32_t temp_raw;
+    int32_t pres_raw;
+	int32_t var1;
+    int32_t var2;
+    int32_t t_fine;
+	
+	/*read the raw ADC data from the I2C registers*/
+	bmp280_read_mem(BMP280_PRES_REG, raw_data, BMP280_RAWDATA_BYTES);
+	pres_raw = bmp280_pack_20bit(raw_data[0], raw_data[1], raw_data[2]);
+	temp_raw = bmp280_pack_20bit(raw_data[3], raw_data[4], raw_data[5]);
 
-	// The following code is based on a 32-bit integer code
-	// from the BMP280 datasheet
+	/*The following code is based on a 32-bit integer code from the BMP280 datasheet*/
 
-	// compute the temperature
+	/*compute the temperature*/
 	var1 = ((((temp_raw >> 3) - ((int32_t)bmp280_cal.dig_t1 << 1)))
 		* ((int32_t)bmp280_cal.dig_t2)) >> 11;
 	var2 = (((((temp_raw >> 4) - ((int32_t)bmp280_cal.dig_t1))
 		* ((temp_raw >> 4) - ((int32_t)bmp280_cal.dig_t1))) >> 12)
 		* ((int32_t)bmp280_cal.dig_t3)) >> 14;
 	t_fine = var1 + var2;
-	_bmp280_temp = (t_fine * 5 + 128) >> 8;
+	bmp280_data.temperature = (t_fine * 5 + 128) >> 8;
 
-	// compute the pressure
+	/*compute the pressure*/
 	var1 = (((int32_t)t_fine) >> 1) - (int32_t)64000;
 	var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * ((int32_t)bmp280_cal.dig_p6);
 	var2 = var2 + ((var1 * ((int32_t)bmp280_cal.dig_p5)) << 1);
@@ -152,35 +166,17 @@ void bmp280_measure(void)
 	var1 = ((((32768 + var1)) * ((int32_t)bmp280_cal.dig_p1)) >> 15);
 
 	if (var1 == 0) {
-		_bmp280_pres = 0;
+		bmp280_data.pressure = 0;
 	} else {
-		_bmp280_pres = (((uint32_t)(((int32_t)1048576)-pres_raw)
+		bmp280_data.pressure = (((uint32_t)(((int32_t)1048576)-pres_raw)
 			- (var2 >> 12))) * 3125;
-		if (_bmp280_pres < 0x80000000) {
-			_bmp280_pres = (_bmp280_pres << 1) / ((uint32_t)var1);
+		if (bmp280_data.pressure < 0x80000000) {
+			bmp280_data.pressure = (bmp280_data.pressure << 1) / ((uint32_t)var1);
 		} else {
-			_bmp280_pres = (_bmp280_pres / (uint32_t)var1) * 2;
+			bmp280_data.pressure = (bmp280_data.pressure / (uint32_t)var1) * 2;
 		}
-		var1 = (((int32_t)bmp280_cal.dig_p9) * ((int32_t)(((_bmp280_pres>>3) * (_bmp280_pres >> 3)) >> 13))) >> 12;
-		var2 = (((int32_t)(_bmp280_pres >> 2)) * ((int32_t)bmp280_cal.dig_p8)) >> 13;
-		_bmp280_pres = (uint32_t)((int32_t)_bmp280_pres + ((var1 + var2 + bmp280_cal.dig_p7) >> 4));
+		var1 = (((int32_t)bmp280_cal.dig_p9) * ((int32_t)(((bmp280_data.pressure>>3) * (bmp280_data.pressure >> 3)) >> 13))) >> 12;
+		var2 = (((int32_t)(bmp280_data.pressure >> 2)) * ((int32_t)bmp280_cal.dig_p8)) >> 13;
+		bmp280_data.pressure = (uint32_t)((int32_t)bmp280_data.pressure + ((var1 + var2 + bmp280_cal.dig_p7) >> 4));
 	}
-}
-
-
-float bmp280_gettemperature(void) {
-	bmp280_measure();
-	return (float)_bmp280_temp / 100;
-}
-
-float bmp280_getpressure(void) {
-	bmp280_measure();
-	return (float)_bmp280_pres / 100;
-}
-
-double bmp280_getaltitude(void) {
-	double alt;
-	bmp280_measure();
-	alt = (1 - pow(_bmp280_pres/(double)101325, 0.1903)) / 0.0000225577;
-	return (alt/10);
 }
